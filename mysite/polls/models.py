5dd83ca39490ff1b910d5b4a86536ab64a618932
abelardo <abbbekgb@gmail.com>
2012-06-27 16:20:20 -0400

    segunda parte do tutorial

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -14,6 +14,13 @@ class Poll(models.Model):
     	return self.question
     def was_published_recently(self):
     	return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    # informa como o campo representado pelo metodo was_published_recently deve ser apresentado
+    # qual atributo deve ser utilizado para ordena-lo
+    was_published_recently.admin_order_field = 'pub_date'
+    # informa que o campo eh booleano(fica um negocio verde bonito)
+    was_published_recently.boolean = True
+    # informa como o campo eh descrito(default eh o nome do metodo sem os _ )
+    was_published_recently.short_description = "Published recently?"
 
 class Choice(models.Model):
 	# usando chave estrangeira/ relacionamento
