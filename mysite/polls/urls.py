f917d7260fb789d25bd97a83737fe2265cce124d
abelardo <abbbekgb@gmail.com>
2012-06-28 16:51:55 -0400

    quarta parte do tutorial

---
+++ mysite/polls/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, url
+
+
+# separando o prefixo polls.views
+urlpatterns = patterns('polls.views',
+    url(r'^$', 'index'),
+    url(r'^(?P<poll_id>\d+)/$', 'detail'),
+    url(r'^(?P<poll_id>\d+)/results/$', 'results'),
+    url(r'^(?P<poll_id>\d+)/vote/$', 'vote'),
+)
